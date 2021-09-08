26359490ae2250c55ed04a3fb8144663128f198a
Douglas <douglasfmr1@gmail.com>
2021-09-08 15:49:06 -0300

    exercicios

---
+++ conteudos/javascript/scripts1c.js
@@ -0,0 +1,25 @@
+document.querySelector("#elementoOndeVoceEsta").parentNode.style.backgroundColor = "red"
+
+document.querySelector("#primeiroFilhoDoFilho").innerHTML = "texxxxxtanuuu"
+
+document.querySelector("#pai").firstElementChild
+
+document.querySelector("#elementoOndeVoceEsta").previousElementSibling
+
+
+let pai = document.querySelector("#pai")
+let filho = document.createElement('section')
+filho.innerHTML = "dsfdsfdsfs"
+filho.className = 'vai-filhao'
+
+pai.appendChild(filho)
+
+let onde = document.querySelector("#elementoOndeVoceEsta")
+let filho2 = document.createElement('div')
+filho2.innerHTML = "rerrerrrew"
+filho2.className = 'tafarel'
+
+onde.appendChild(filho2)
+
+let inList = document.querySelectorAll()
+
